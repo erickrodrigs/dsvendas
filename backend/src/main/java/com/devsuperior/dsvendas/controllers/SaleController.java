598162e37e769f660f3374dbd16e95f35750728d
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-08-02 23:58:36 -0300

    add custom queries: amount by seller and success by seller
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.services.SaleService;
 
 @RestController
@@ -22,5 +26,17 @@ public class SaleController {
   public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
     Page<SaleDTO> sales = service.findAll(pageable);
     return ResponseEntity.ok(sales);
+  }
+  
+  @GetMapping(value = "/amount-by-seller")
+  public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
+    List<SaleSumDTO> amountBySeller = service.amountGroupedBySeller();
+    return ResponseEntity.ok(amountBySeller);
+  }
+  
+  @GetMapping(value = "/success-by-seller")
+  public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+    List<SaleSuccessDTO> successBySeller = service.successGroupedBySeller();
+    return ResponseEntity.ok(successBySeller);
   }
 }
